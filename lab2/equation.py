af540ac79040cd710e57c082b91402e9e40a589b
Roman <roman@sumjest.ru>
2021-09-17 00:14:15 +0500

    lab2 complete

---
+++ lab2/equation.py
@@ -0,0 +1,18 @@
+import math
+
+number = int(input("Введите номер функции(1-4): "))
+x = float(input("Введите значение x: "))
+
+try:
+    if number == 1:
+        print("y = " + str(1 / (math.sqrt(1 - x ** 4 - x ** 2))))
+    elif number == 2:
+        print("y = " + str(math.pow(math.e, math.sqrt(x + math.sqrt(x)))))
+    elif number == 3:
+        print("y = " + str((x**2 +1) / (3*(x**2 - 1)) + (x**2 - 1) * (1-x)))
+    elif number == 4:
+        print("y = " + str(math.sqrt(x + math.sqrt(x + math.sqrt(x)))))
+    else:
+        print("Номер неверный ;-(")
+except (ZeroDivisionError, ValueError):
+    print("x не входит в область допустимых значений")
---
+++ lab2/geometry.py
@@ -0,0 +1,20 @@
+from math import sqrt
+from math import sin
+
+number = int(input("Введите номер задачи(1-4): "))
+a = float(input("Введите значение a: "))
+
+try:
+    if number == 1:
+        print("Радиус сферы равен: " + str(a*sqrt(3)/2))
+    elif number == 2:
+        print("Радиус сферы равен: " + str((a/(4*sqrt(3)))*(3+sqrt(5))))
+    elif number == 3:
+        print("Радиус сферы равен: " + str((a/4)*sqrt(2*(5+sqrt(5)))))
+    elif number == 4:
+        r = input("Введите радиус вписанной окружности: ")
+        print("Площадь равнобедренной трапеции равна: " + str(4*(r**2)/sin(a)))
+    else:
+        print("Номер неверный ;-(")
+except (ZeroDivisionError, ValueError):
+    print("Введенные значения не входят в область допустимых значений")
