365f7f588199e2fc5e9bbc19a388b2bffba97078
Roman <roman@sumjest.ru>
2021-09-26 13:16:13 +0500

    lab2 final complete

---
+++ lab2/2.2.py
@@ -0,0 +1,24 @@
+# Целые числа и числа с плавающей точкой являются одними из самых распространенных в языке Python
+
+number = 9
+
+print(type(number))   # Вывод типа переменной number
+
+float_number = 9.0
+
+# Создайте ещё несколько переменных разных типов и осуществите вывод их типов
+
+integer_number = 3
+
+string_number = "4.123"
+
+print(str(float_number) + " (" + str(type(float_number)) + ")\n" + str(integer_number) + " ("+ str(type(integer_number)) + ")\n" + string_number + " (" + str(type(string_number)) + ")\n")
+
+# Существует множество функций, позволяющих изменять тип переменных.
+# Изучите такие функции как int(), float(), str() и последовательно примените их к переменным, созданным ранее.
+string_number = float(string_number)
+print(string_number)
+string_number = int(string_number)
+print(string_number)
+string_number = str(string_number)
+print(string_number)
---
+++ lab2/2.4.py
@@ -0,0 +1,23 @@
+# Составной оператор присваивания объединяет в себе простое присваивание и арифметическую бинарную операцию (+=, -= и т.д).
+
+number = 9.0
+
+print("number = " + str(number))
+
+number -= 2
+
+print("number = " + str(number))
+
+# Попрактикуйтесь в использовании разных типов составных операторов для переменной number, выведите результаты в консоль
+
+number*=2
+
+print("number = " + str(number))
+
+number+=1
+
+print("number = " + str(number))
+
+number/=3
+
+print("number = " + str(number))
