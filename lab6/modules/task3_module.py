75754b7bae3ddb46e2b9b38d3c645974038fba82
Roman <roman@sumjest.ru>
2021-11-12 12:56:45 +0500

    update task3.py

--- lab6/modules/task3_module.py
+++ lab6/modules/task3_module.py
@@ -1,7 +1,7 @@
 import os
 
 
-def input_exist():
+def input_exist() -> bool:
     for file in os.listdir(os.getcwd() + "\\lab6"):
         if file.endswith(".txt"):
             if file == "input.txt":
@@ -9,21 +9,32 @@ def input_exist():
     return False
 
 
-def read_n():
+def read_n() -> int:
     f = open("lab6/input.txt", 'r')
     n = int(f.readline().split(' ')[0])
     f.close()
     return n
 
 
-def is_prime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def prime_filter(list_of_integer: list) -> list:
+    """
+    Реализация алгоритма Решето Эратосфена
+    :param list_of_integer: List of integer numbers
+    :return: List of prime numbers
+    """
+    count = len(list_of_integer)
+    list_of_integer = list(range(2, count + 1))  # список целых чисел до count
+    i_num = 0  # Индекс текущего числа, кратные которому будут исключатся из массива
+    while list_of_integer[i_num] ** 2 <= count:
+        for i in range(list_of_integer[i_num] ** 2, count + 1,
+                       list_of_integer[i_num]):  # Пробегаемся по всем числам, кратным выбранному
+            if i in list_of_integer:
+                list_of_integer.remove(i)
+        i_num += 1  # берем следующее не исключенное число
+    return list_of_integer
 
 
-def write_line(line):
+def write_line(line: str):
     f = open("lab6/output.txt", 'w')
     f.write(line)
     f.close()
--- lab6/task3.py
+++ lab6/task3.py
@@ -2,14 +2,15 @@ from lab6.modules import task3_module
 
 if not task3_module.input_exist():
     print("Файл с входными данными не обнаружен")
-    exit()
+    exit(0)
 
-n = task3_module.read_n()
+count = task3_module.read_n()
 
-prime_numbers = ""
+list_of_integers = list(range(2, count))
 
-for i in range(2, n + 1):
-    if task3_module.is_prime(i):
-        prime_numbers += str(i) + " "
+prime_numbers = task3_module.prime_filter(list_of_integers)
 
-task3_module.write_line(prime_numbers)
+output = " ".join(
+    map(str, prime_numbers))  # преобразовываем числа в списке в строковый формат и заполняем строку элементами
+
+task3_module.write_line(output)  # выводим в файл
